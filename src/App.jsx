374f5677d2968b387d2748823530a5207ec70d58
Victor <viktorsorlov@gmail.com>
2019-04-16 16:49:25 +0300

    create component Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import s from './App.module.css';
 import HomePage from './pages/home-page';
 import MarkedPage from './pages/marked-page';
+import Header from './organisms/header';
 
 const App = () => (
   <div className={s.wrapper}>
     <BrowserRouter>
+      <React.Fragment>
+        <Header />
         <Switch>
           <Route path="/" component={ HomePage } exact />
-          <Route path="/search-result/:id" component={ MarkedPage } exact />
+          <Route path="/markers" component={ MarkedPage } exact />
         </Switch>
+      </React.Fragment>
     </BrowserRouter>
   </div>
 );
---
+++ src/organisms/header/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import s from './Header.module.css';
+import logo from '../../images/logo.png';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+
+const Header = () => (
+  <Navbar 
+    bg="dark" 
+    variant="dark" 
+    fixed="top" 
+    expand="md">
+  
+    <Navbar.Brand href="#home">
+      <img
+        src={logo}
+        width="90"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
+
+    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+      <Nav>
+        <Link className={s.navLinks} to="/">
+          <Nav.Item>Фильмы</Nav.Item>
+        </Link>
+        <Link className={s.navLinks} to="/markers">
+          <Nav.Item>Закладки</Nav.Item>
+        </Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default Header;
